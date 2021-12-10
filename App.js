4d276c96f576cb2a084f0a2c8041f64f56a71832
unknown <djl.belo@campus.fct.unl.pt>
2021-12-10 15:20:01 +0000

    segundo commit

--- App.js
+++ App.js
@@ -1,21 +1,54 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { Image, StyleSheet, Text, TouchableOpacity, View } from 'react-native';
+import Expo from 'expo';
+import {Scene, Mesh, MeshBasicMaterial, PerspectiveCamera, BoxBufferGeometry} from 'three';
+import { Renderer, TextureLoader, THREE } from 'expo-three';
+import * as GL from 'expo-gl';
 
-export default function App() {
+const App = () => {
+
+  const onContextCreate = async (gl) =>{
+    //THREE.js
+
+    const scene = new THREE.Scene()
+    const camera = new THREE.PerspectiveCamera(
+      75,
+      gl.drawingBufferWidth/gl.drawingBufferHeight,
+      0.1,
+      1000
+    )
+
+    gl.canvas = {width: gl.drawingBufferWidth, heigth: gl.drawingBufferHeight}
+    camera.position.z = 2
+
+    const renderer = new Renderer({gl})
+    renderer.setSize(gl.drawingBufferWidth, gl.drawingBufferHeight)
+
+    const geometry = new BoxBufferGeometry(1,1,1)
+    const material = new MeshBasicMaterial({
+      color: 'red'
+    })
+    const cube = new Mesh(geometry, material)
+    scene.add(cube)
+
+    const render = () => {
+      requestAnimationFrame(render)
+      cube.rotation.x += 0.01
+      cube.rotation.y += 0.01
+      renderer.render(scene, camera)
+      gl.endFrameEXP()
+    }
+    render()
+  }
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style = {{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
+      <GL.GLView     
+        style ={{width:500, heigth:500}}
+        onContextCreate = {onContextCreate}
+      />
     </View>
-  );
+  )
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+export default App
